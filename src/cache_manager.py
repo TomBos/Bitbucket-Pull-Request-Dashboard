849a27fe64af3b703a0182a945fa1c2a497d8b95
TomBos <tombos255@gmail.com>
2025-06-13 08:50:06 +0200

    Cache manager

---
+++ src/cache_manager.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import json
+
+class cacheManager():
+    def __init__(self):
+        pass
+
+
+    def validate_json_cache(self, filepath):
+
+        return
+
+
+
+
+
