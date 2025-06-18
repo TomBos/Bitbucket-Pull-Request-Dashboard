f3423d936e01612727e53f779162330efd078602
TomBos <tombos255@gmail.com>
2025-06-18 17:49:57 +0200

    Working backend server

--- src/http_server.py
+++ src/http_server.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from typing import Union, Dict, Any, Optional
+from dotenv import load_dotenv
 
 from request_controller import RequestController
 from cache_manager import CacheManager
-from dotenv import load_dotenv
 
 import json
 import os
 import sys
 import time
 import datetime
+import glob
 
 APPROVAL_THRESHOLD = 2
 CACHE_DIR = "cache/"
 CACHE_TTL = 3600
 
-def load_env_vars(required_keys):
+def load_env_vars(*required_keys: str):
     load_dotenv()
     env = {}
     for key in required_keys:
@@ -26,13 +28,18 @@ def load_env_vars(required_keys):
         env[key] = value
     return env
 
-config = load_env_vars(["API_KEY", "API_USER", "ORGANIZATION", "PROJECT"])
+config = load_env_vars("API_KEY", "API_USER", "ORGANIZATION", "PROJECT")
+
 
 class ServerController(BaseHTTPRequestHandler):
-    def _set_headers(self, status=200, content_type="application/json"):
+    
+    def _send_response(self, response_object: Union[str, bytes], status: int = 200, content_type: str = "application/json") -> None:
         self.send_response(status)
         self.send_header("Content-type", content_type)
         self.end_headers()
+        if isinstance(response_object, str):
+            response_object = response_object.encode()
+        self.wfile.write(response_object)
 
     def do_GET(self):
         if self.path == "/reload-cache":
@@ -45,8 +52,9 @@ class ServerController(BaseHTTPRequestHandler):
                 cache_expired = True
 
             if not cache_expired:
-                return
-            
+                response_json = json.dumps({"message": "cache has been updated within last hour"}) + "\n"
+                return self._send_response(response_json)
+ 
             cache = CacheManager()
             cache.clear_pr_cache(CACHE_DIR) 
     
@@ -56,10 +64,11 @@ class ServerController(BaseHTTPRequestHandler):
             try:
                 overview_response = req.get(base_endpoint)
                 if not overview_response:
-                    return
+                    response = json.dumps({"message": "Bitbucket API returned empty object"}) + "\n"
+                    return self._send_response(response, 500)
             except Exception as e:
-                print(f"Failed to fetch pull requests overview: {e}")
-                return
+                response = json.dumps({"message": f"{e}"}) + "\n"
+                return self._send_response(response, 500)
 
             cache.save_cache(overview_response, pr_overview_path)
             pull_requests = overview_response.get("values", [])
@@ -73,8 +82,8 @@ class ServerController(BaseHTTPRequestHandler):
                 try:
                     pr_details = req.get(pr_endpoint)
                 except Exception as e:
-                    print(f"Failed to fetch PR {pr_id}: {e}")
-                    continue
+                    response = json.dumps({"message": f"Failed to fetch PR {pr_id}: {e}"}) + "\n"
+                    return self._send_response(response, 500)
 
                 if not cache.has_enough_approvals(pr_details, APPROVAL_THRESHOLD):
                     parsed_data = cache.trim_cache_object(pr_details)
@@ -82,10 +91,20 @@ class ServerController(BaseHTTPRequestHandler):
                     cache.save_cache(parsed_data, pr_cache_path)
 
 
+        if self.path == "/tea":
+            response = json.dumps({"error": "I'm a teapot 🫖 - I cannot brew coffee"}) + "\n"
+            return self._send_response(response)
 
 
-        if self.path == "/server-content":
-           return 
+        if self.path == "/serve-content":
+            response_object = []
+            for file in glob.glob(os.path.join(CACHE_DIR, "*.json")):
+                cache = CacheManager()
+                data = cache.load_cache(file)
+                response_object.append(data)
+            response_json = json.dumps(response_object) + "\n"
+
+            return self._send_response(response_json)
 
 
 def run(server_class=HTTPServer, handler_class=ServerController, port=8000):
