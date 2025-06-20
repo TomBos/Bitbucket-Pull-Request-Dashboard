25f01a15f761b637a20d08c82159167e05498058
TomBos <tombos255@gmail.com>
2025-06-20 08:58:13 +0200

    Node gitignore

--- src/vite/src/App.tsx
+++ src/vite/src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
+import { Button } from "@/components/ui/button"
 
+
+function App() {
   return (
     <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button variant="outline">Button</Button>
+      </div>
     </>
   )
 }
