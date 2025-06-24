3691eac87441cdc23f0ff4cc81ffa05062a938a0
TomBos <tombos255@gmail.com>
2025-06-24 09:18:12 +0200

    Lib tracking

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -4,24 +4,23 @@ import { useEffect, useState } from "react"
 function App() {
   const [data, setData] = useState(null)
 
-useEffect(() => {
-  fetch("/cache/6829.pr.json")
-    .then(async (res) => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`)
-      }
-      const text = await res.text()
-      try {
-        const json = JSON.parse(text)
-        setData(json)
-      } catch (e) {
-        console.error("JSON parse error:", e, "Response text:", text)
-      }
-    })
-    .catch((err) => {
-      console.error("Fetch error:", err)
-    })
-}, [])
+  useEffect(() => {
+    fetch("/api")
+      .then(async (res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`)
+        }
+        const text = await res.text()
+        try {
+          const json = JSON.parse(text)
+          setData(json)
+        } catch (e) {
+          console.error("JSON parse error:", e, "Response text:", text)
+        }
+      }).catch((err) => {
+        console.error("Fetch error:", err)
+      })
+  }, [])
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center px-[10%]">
